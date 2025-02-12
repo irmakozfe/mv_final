af7a6da6f5b81cead21491388e33b57803870095
Irmak Damla Özdemir <123468140+irmakozfe@users.noreply.github.com>
2025-02-12 01:19:16 +0100

    Delete mv_command_final directory

--- mv_command_final/include/file_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILE_UTILS_H
-#define FILE_UTILS_H
-
-int file_exists(const char *filename);
-int delete_file(const char *filename);
-int is_file(const char *path);
-
-#endif
--- mv_command_final/include/move_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOVE_UTILS_H
-#define MOVE_UTILS_H
-
-int mvRenameFile(const char *source_file, const char *new_name);
-int mvRenameDir(const char *source_dir, const char *new_name);
-int mvMoveFileToDir(const char *source, const char *destination_dir);
-int mvMoveDirToDir(const char *source_dir, const char *target_dir);
-
-#endif //MOVE_UTILS_H
