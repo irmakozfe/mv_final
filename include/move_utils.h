39cda7ce51efa43c0060d96e1fa7b3a40b450a62
Irmak Damla Özdemir <123468140+irmakozfe@users.noreply.github.com>
2025-02-14 19:24:53 +0100

    Update move_utils.h

--- include/move_utils.h
+++ include/move_utils.h
@@ -5,5 +5,6 @@ int mvRenameFile(const char *source_file, const char *new_name);
 int mvRenameDir(const char *source_dir, const char *new_name);
 int mvMoveFileToDir(const char *source, const char *destination_dir);
 int mvMoveDirToDir(const char *source_dir, const char *target_dir);
+void print_help();
 
 #endif 
